d2111c1c0b298cfa0214c016cd3479ea300c83cb
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-01-29 19:22:21 +0530

    add functinality for Error handling  in wrong URL.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const morgan = require('morgan');
 const productRouter = require(`${__dirname}/routes/productRoutes.js`);
 const userRouter = require(`${__dirname}/routes/userRoutes.js`);
 
+const AppError = require(`${__dirname}/utils/appError.js`);
+const GlobalErrorHandler = require(`${__dirname}/controllers/errorController.js`);
+
 // console.log(app.get('env')); //! to get Environment if it is dev or production
 
 if (process.env.NODE_ENV === 'development') {
@@ -14,11 +17,13 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`)); //!Serving static file in route
 
+//! Sample to check request
 app.use((Request, Response, next) => {
   console.log('Hello From ॐ  Middleware!');
   next();
 });
 
+//! To get the time of request
 app.use((Request, Response, next) => {
   Request.requestTime = new Date().toISOString();
   next();
@@ -29,8 +34,16 @@ const blankRoute = (Request, Response) => {
   Response.send('Fuck You....!!!');
 };
 
+//! Routes Middleware
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
 app.route('/').get(blankRoute);
+
+//! Error handling for wrong URL
+app.all('*', (Request, Response, next) => {
+  next(new AppError(`Can Not Find ${Request.originalUrl} URL in Server!`, 404));
+});
+
+app.use(GlobalErrorHandler);
 
 module.exports = app;
