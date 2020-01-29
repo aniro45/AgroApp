e9a1150bbde071e7dbf825c0cd5684f8035d40f8
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-01-29 19:24:27 +0530

    error handling for uncaught and unhandled errors.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', error => {
+  console.log('Uncaught Exception! Shutting Down...');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require(`${__dirname}/app.js`);
 
@@ -18,33 +24,37 @@ const DB = process.env.DATABASE_ONLINE.replace(
 // const DB = process.env.DATABASE_OFFLINE;
 
 //! MONGOOSE FUNCTION TO CONNECT DATABASE
-try {
-  mongoose
-    .connect(DB, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    })
-    .then(con => {
-      if (
-        DB ===
-        process.env.DATABASE_ONLINE.replace(
-          '<PASSWORD>',
-          process.env.DB_PASSWORD
-        )
-      ) {
-        console.log('CONNECTED TO ONLINE AgroApp DATABASE.');
-      } else {
-        console.log('CONNECTED TO OFFLINE AgroApp DATABASE.');
-      }
-    });
-} catch (error) {
-  console.log('Check Internet Connection or Error Log for More Info!' + error);
-}
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => {
+    if (
+      DB ===
+      process.env.DATABASE_ONLINE.replace('<PASSWORD>', process.env.DB_PASSWORD)
+    ) {
+      console.log('CONNECTED TO ONLINE AgroApp DATABASE.');
+    } else {
+      console.log('CONNECTED TO OFFLINE AgroApp DATABASE.');
+    }
+  });
 
 //! SERVER CODE
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is Running on port ${port}...`);
+  console.log(process.env.NODE_ENV);
 });
+
+process.on('unhandledRejection', error => {
+  console.log(error.name, error.message);
+  console.log('UNdandler Rejection! Shutting Down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+
