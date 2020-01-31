d28a0b0c93826874bd9c432e1a9a9773f742649f
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-01-31 16:12:02 +0530

    updated getAllUser with error handling and full functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,24 @@
-const fs = require('fs');
+const Products = require(`${__dirname}/../models/productModel.js`);
+const APIFeatures = require(`${__dirname}/../utils/apiFeatures.js`);
+const catchAsync = require(`${__dirname}/../utils/catchAsync.js`);
+const User = require(`${__dirname}/../models/userModel`);
 
-const readJosnFile = fs.readFileSync(
-  `${__dirname}/../devData/data/userData.json`,
-  'utf-8'
-);
+// const readJosnFile = fs.readFileSync(
+//   `${__dirname}/../devData/data/userData.json`,
+//   'utf-8'
+// );
 
-exports.getAllUsers = (Request, Response) => {
-  Response.status(500).json({
-    status: 'error',
-    message: 'This User route is not yet defined!'
+exports.getAllUsers = catchAsync(async (Request, Response) => {
+  const allUsers = await User.find();
+  Response.status(200).json({
+    Status: 'Successfull',
+    RequestedAt: Request.requestTime,
+    results: allUsers.length,
+    data: {
+      allUsers
+    }
   });
-};
+});
 
 exports.createNewUser = (Request, Response) => {
   Response.status(500).json({
