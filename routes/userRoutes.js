3db77ec93a32716aba8bc68e4300d7aa5744dc59
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-02-02 03:36:28 +0530

    added routes for forgotPassword, resetPassword,updateMyPassword,updateMe,deleteMe

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,9 +4,24 @@ const authController = require(`${__dirname}/../controllers/authController.js`);
 
 const router = express.Router();
 
+//! Signin Login Route
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
+//! Forgot password related Route
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+//! Route to handle operations while user is logged in
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+//! Basic Routes
 router
   .route('/')
   .get(userController.getAllUsers)
