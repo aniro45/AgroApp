4b0f63d56f1d6f35e3be3c718ff78ab9b02a0285
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-02-03 03:16:15 +0530

    changed enum of role from dev to seller

--- models/userModel.js
+++ models/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
   photo: String,
   role: {
     type: String,
-    enum: ['user', 'admin', 'dev'],
+    enum: ['user', 'admin', 'seller'],
     default: 'user'
   },
   password: {
