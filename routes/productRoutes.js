cdd4b7d4e86da64bab5cd4a2148ced7bc493443d
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-02-06 20:20:59 +0530

    Review Router middlware to use intial route string

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const productController = require(`${__dirname}/../controllers/productController.js`);
 const authController = require(`${__dirname}/../controllers/authController`);
+const reviewRouter = require(`${__dirname}/../routes/reviewRoutes`);
+
 const router = express.Router();
 
 // router.param('id', productController.checkId);
+
+//POST /products/213458dfdf/reviews
+//GET /products/213458dfdf/reviews
+//GET /products/213458dfdf/reviews/215df5ef5
+
+// router
+//   .route('/:productId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+router.use('/:productId/reviews', reviewRouter);
 
 //! Special Route for top 5 cheap Products
 router
@@ -31,4 +46,5 @@ router
     authController.restrictTo('admin', 'seller'),
     productController.deleteProduct
   );
+
 module.exports = router;
