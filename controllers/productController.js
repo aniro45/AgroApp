a84ac86206a0f3e63696fdfdc5a0660118e55f11
aniket jadhav <aniket.jadhav.8151@gmail.com>
2020-02-07 17:50:59 +0530

    removed all the sphageti code which moved to handler function for generelization

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 //Testing Purpose
 const Products = require(`${__dirname}/../models/productModel.js`);
 const APIFeatures = require(`${__dirname}/../utils/apiFeatures.js`);
-const catchAsyncError = require(`${__dirname}/../utils/catchAsync.js`);
+const catchAsync = require(`${__dirname}/../utils/catchAsync.js`);
 const AppError = require(`${__dirname}/../utils/appError.js`);
 const factory = require(`${__dirname}/handlerFactory`);
 
@@ -15,99 +15,28 @@ exports.aliasCheapProducts = (Request, Response, next) => {
   next();
 };
 
-//! Get All Products
-exports.getAllproducts = catchAsyncError(async (Request, Response, next) => {
-  console.log(process.env.NODE_ENV);
-
-  const features = new APIFeatures(Products.find(), Request.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const allProducts = await features.query;
-
-  //! Send Respose
-  Response.status(200).json({
-    Status: 'Successfull',
-    RequestedAt: Request.requestTime,
-    results: allProducts.length,
-    data: {
-      allProducts
-    }
-  });
+//! Get Full list of
+exports.allProduxes = catchAsync(async (Request, Response, next) => {
+  const getFullList = Products.find();
 });
+
+//! Get All Products
+exports.getAllproducts = factory.getAll(Products);
 
 //! Create New Product
-exports.createNewProduct = catchAsyncError(async (Request, Response, next) => {
-  const newProduct = await Products.create(Request.body);
-  Response.status(201).json({
-    Status: 'Successfull',
-    RequestedAt: Request.requestTime,
-    data: {
-      FileData: newProduct
-    }
-  });
-});
+exports.createProduct = factory.createOne(Products);
 
 //! Get Single Product By ID
-exports.getProduct = catchAsyncError(async (Request, Response, next) => {
-  const product = await Products.findById(Request.params.id).populate(
-    'reviews'
-  );
-  //Product.findOne({_id:Request.parma.id})
-
-  if (!product) {
-    return next(new AppError('No Product found with this ID!', 404));
-  }
-  Response.status(404).json({
-    message: 'Success',
-    RequestedAt: Request.requestTime,
-    data: {
-      product
-    }
-  });
-});
+exports.getProduct = factory.getOne(Products, { path: 'reviews' });
 
 //! update the Patch for the Product
-exports.patchProduct = catchAsyncError(async (Request, Response, next) => {
-  const patchedProduct = await Products.findByIdAndUpdate(
-    Request.params.id,
-    Request.body,
-    {
-      new: true,
-      runValidators: true
-    }
-  );
-  if (!patchedProduct) {
-    return next(new AppError('No Product found with this ID!', 404));
-  }
-  Response.status(200).json({
-    message: 'Patched Success',
-    RequestedAt: Request.requestTime,
-    data: {
-      patchedProduct
-    }
-  });
-});
+exports.patchProduct = factory.updateOne(Products);
 
 //! Delete Product By id
-exports.deleteProduct = factory.deleteOne(product);
-
-// exports.deleteProduct = catchAsyncError(async (Request, Response, next) => {
-//   const product = await Products.findByIdAndDelete(Request.params.id);
-//   if (!product) {
-//     return next(new AppError('No Product found with this ID!', 404));
-//   }
-//   Response.status(204).json({
-//     message: 'Delete Success',
-//     RequestedAt: Request.requestTime,
-//     data: null
-//   });
-// });
+exports.deleteProduct = factory.deleteOne(Products);
 
 //! Aggregation Pipeline
-
-exports.productStats = catchAsyncError(async (Request, Response, next) => {
+exports.productStats = catchAsync(async (Request, Response, next) => {
   const stats = await Products.aggregate([
     {
       $match: { rating: { $gte: 4 } }
