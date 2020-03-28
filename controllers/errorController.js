f87d78091c1a6cb37c62e721047f33f4b1f2aadb
aniro45 <aniket.jadhav.8151@gmail.com>
2020-03-28 10:19:50 +0530

    Error handling for Rendered website. Now error are handled for rendered website also like API for both Developer and production enviornment. Fixed some bugs that causes misfuction in prodution enviornment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -23,47 +23,78 @@ const handleJWTError = () =>
 const handleJWTExpiredError = () =>
   new AppError('Your Token has expired, please log in again', 401);
 
-const sendErrorDev = (error, Response) => {
-  Response.status(error.statusCode).json({
-    status: error.status,
-    error: error,
-    message: error.message,
-    stack: error.stack
+const sendErrorDev = (error, Request, Response) => {
+  // a) API
+  if (Request.originalUrl.startsWith('/api')) {
+    return Response.status(error.statusCode).json({
+      status: error.status,
+      error: error,
+      message: error.message,
+      stack: error.stack
+    });
+  }
+  // b) THE RENDERED WEBSITE
+  console.log('Error', error);
+  return Response.status(error.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: error.message
   });
 };
 
-const sendErrorProd = (error, Response) => {
-  //Opeartional, Trusted error : send msg to client.
-  if (error.isOperational) {
-    console.log('isOperation runining');
-    Response.status(error.statusCode).json({
-      status: error.status,
-      message: error.message
-    });
-    //programming Or other unkonwn errors : dont leak error details
-  } else {
+const sendErrorProd = (error, Request, Response) => {
+  // a) API
+  if (Request.originalUrl.startsWith('/api')) {
+    //a) Opeartional, Trusted error : send msg to client.
+    if (error.isOperational) {
+      return Response.status(error.statusCode).json({
+        status: error.status,
+        message: error.message
+      });
+    }
+    // b) programming Or other unkonwn errors : dont leak error details
     //Log Error
     console.log('Error', error);
+
     //Send generic message
-    Response.status(500).json({
+    return Response.status(500).json({
       status: 'error',
-      message: 'something went Wrong!'
+      message: 'Something went wrong!'
     });
   }
+  // b) RENDERED WEBSITE
+  if (error.isOperational) {
+    console.log(error);
+    return Response.status(error.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: error.message
+    });
+  }
+  //programming Or other unkonwn errors : dont leak error details
+  //Log Error
+  console.log('Error', error);
+
+  //Send generic message
+  return Response.status(statusCode).render('error', {
+    title: 'Something Went wrong!',
+    msg: 'Please Try again leter!'
+  });
 };
+
 module.exports = (error, Request, Response, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(error, Response);
+    sendErrorDev(error, Request, Response);
   } else if (process.env.NODE_ENV === 'production') {
     let err = { ...error };
+    err.message = error.message;
+
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
     if (err.name === 'ValidationError') err = handleValidationErrorDb(err);
 
     if (err.name === 'JsonWebTokenError') err = handleJWTError();
     if (err.name === 'TokenExpiredError') err = handleJWTExpiredError();
-    sendErrorProd(err, Response);
+    sendErrorProd(err, Request, Response);
   }
 };
