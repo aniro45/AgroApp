0386c927d755949d31341733b96f208c54d0a318
aniro45 <aniket.jadhav.8151@gmail.com>
2020-03-28 10:22:57 +0530

    Intital Commit for ViewController. View controller handles all the frontEnd Events in functions. Diiferent functions for login logout update user data and user password.

---
+++ controllers/viewsController.js
@@ -0,0 +1,74 @@
+const Product = require(`${__dirname}/../models/productModel`);
+const User = require(`${__dirname}/../models/userModel`);
+const Review = require(`${__dirname}/../models/reviewModel`);
+const catchAsync = require(`${__dirname}/../utils/catchAsync`);
+const AppError = require(`${__dirname}/../utils/appError`);
+
+exports.getOverview = catchAsync(async (Request, Response, next) => {
+  //get product data from collection
+  const products = await Product.find();
+  const reviews = await Review.find();
+
+  //build template
+
+  //render that template using the data from 1
+
+  Response.status(200).render('overview', {
+    title: 'All Products',
+    products
+  });
+});
+
+exports.getProduct = async (Request, Response, next) => {
+  // 1) get the data for requested product(includieng review and salers)
+
+  const product = await Product.findOne({ slug: Request.params.slug }).populate(
+    {
+      path: 'reviews',
+      fields: 'review rating user'
+    }
+  );
+
+  if (!product) {
+    return next(new AppError('There is no product with that name', 404));
+  }
+
+  //2) Build template
+
+  //3) Render Template using the data from step 1 again
+  Response.status(200).render('product', {
+    title: `${product.name}`,
+    product
+  });
+};
+
+exports.getLoginForm = (Request, Response) => {
+  Response.status(200).render('login', {
+    title: 'Log into Your Account'
+  });
+};
+
+exports.getAccount = (Request, Response) => {
+  Response.status(200).render('account', {
+    title: 'Your Account'
+  });
+};
+
+exports.updateUserData = catchAsync(async (Request, Response, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    Request.user.id,
+    {
+      name: Request.body.name,
+      email: Request.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  Response.status(200).render('account', {
+    title: 'Your Account',
+    user: updatedUser
+  });
+ });
