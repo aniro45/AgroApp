63c73ad823d162d61b694bda02e51b22911cb0eb
aniro45 <aniket.jadhav.8151@gmail.com>
2020-04-21 17:49:07 +0530

    added for handling signup form on client side using ajax calls

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+export const signup = async (name, email, password, passwordConfirm) => {
+  try {
+    const result = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name: name,
+        email: email,
+        password: password,
+        passwordConfirm: passwordConfirm,
+      },
+    });
+
+    if (result.data.status === 'success') {
+      showAlert('success', 'Created New Account Successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
